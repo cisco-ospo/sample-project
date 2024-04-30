385fd98ccf8315216ca020b01377c9c69eee4550
lelia <le1ia@me.com>
2024-04-29 20:02:34 -0400

    Add demo func to hello program
    Signed-off-by: lelia <le1ia@me.com>

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
+import requests
+
 def run():
     print('Hello world')
+    demo()
+
+def demo():
+    x = requests.get('https://w3schools.com/python/demopage.htm')
+    print(x.text)
 
 run()
